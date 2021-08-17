a3545383e5ceb0ce00a6e55c1eb07c711205644d
Christopher <cristoferperezmejia80@gmail.com>
2021-08-17 14:56:17 -0400

    alot

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,30 @@
 import Testimonials from "./Testimonials/Testimonials";
 
+import beforeArrow from "./Testimonials/svg/icon-prev.svg";
+import nextArrow from "./Testimonials/svg/icon-next.svg";
+
+import tanyaImage from "./Testimonials/images/image-tanya.jpg";
+import johnImage from "./Testimonials/images/image-john.jpg";
+
+const arrows = [beforeArrow, nextArrow];
+
 const testimonials = [
   {
     userName: "Tanya Sinclair",
+    userImage: tanyaImage,
     userRoll: "UX Engineer",
     userComment: " “ I’ve been interested in coding for a while but never taken the jump, until now.   I couldn’t recommend this course enough. I’m now in the job of my dreams and so  excited about the future.”",
   },
-  { userName: "John Tarkpor", userRoll: "Junior Front-end Developer", userComment: "  “ If you want to lay the best foundation possible I’d recommend taking this course.  The depth the instructors go into is incredible. I now feel so confident about   starting up as a professional developer. ”" },
+  {
+    userName: "John Tarkpor",
+    userImage: johnImage,
+    userRoll: "Junior Front-end Developer",
+    userComment: "  “ If you want to lay the best foundation possible I’d recommend taking this course.  The depth the instructors go into is incredible. I now feel so confident about   starting up as a professional developer. ”",
+  },
 ];
 
 function App() {
-  return <Testimonials props={testimonials} />;
+  return <Testimonials arrows={arrows} testimonials={testimonials} />;
 }
 
 export default App;
--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { createRef, useRef, useEffect } from "react";
+import "./normalize.css";
+import "./Testimonials.css";
 
-const Testimonials = () => {
-  return <div>hola</div>;
+const Testimonials = (props) => {
+  console.log(props);
+  console.log(<TestimonialsImages />);
+
+  const imagesContainer = createRef();
+
+  let cont;
+
+  const handleBtnClick = (e) => {
+    console.log(e.target);
+    if (e.target.className === "beforeArrow") {
+      // e.target..current.style.left = cont + "00%"
+    }
+  };
+
+  console.log(props.arrows);
+  return (
+    <main className="container">
+      <div className="btnContainer">
+        <button className="beforeArrow" onClick={handleBtnClick}>
+          <img src={props.arrows[0]} alt={props.arrows[1]} />
+        </button>
+        <button className="nextArrow" onClick={handleBtnClick}>
+          <img src={props.arrows[1]} alt={props.arrows[1]} />
+        </button>
+      </div>
+      <TestimonialsImages ref={imagesContainer} testimonials={props.testimonials} />
+    </main>
+  );
+};
+
+const TestimonialsImages = (props) => {
+  console.log(props.ref);
+
+  const imagesContainer = props.ref;
+
+  useEffect(() => {
+    console.log(imagesContainer);
+
+    imagesContainer.current.style.width = props.testimonials.length + "00%";
+
+    Array.from(imagesContainer.current.children).forEach((testimonials) => {
+      testimonials.style.width = 100 / props.testimonials.length + "%";
+    });
+  }, [imagesContainer, props]);
+
+  return (
+    <div className="imagesTestimonialsContainer">
+      <div ref={imagesContainer} className="imagesContainer">
+        {props.testimonials.map((testimonial) => {
+          return <img key={testimonial.userImage} src={testimonial.userImage} alt={testimonial.userImage} />;
+        })}
+      </div>
+      <TestimonialsComments />
+    </div>
+  );
+};
+
+const TestimonialsComments = () => {
+  return <div></div>;
 };
 
 export default Testimonials;
